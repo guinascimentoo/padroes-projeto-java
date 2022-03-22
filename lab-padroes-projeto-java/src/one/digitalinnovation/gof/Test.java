317b1254ed395b0cab4031675587bc3ae1b3bc65
Guilherme Nascimento <guilherme_nascimeento18@outlook.com>
2022-03-22 19:14:28 -0300

    projeto dio

---
+++ lab-padroes-projeto-java/src/one/digitalinnovation/gof/Test.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package one.digitalinnovation.gof;
+
+import one.digitalinnovation.gof.facade.Facade;
+import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.singleton.SingletonEager;
+import one.digitalinnovation.gof.strategy.Comportamento;
+import one.digitalinnovation.gof.strategy.ComportamentoAgressivo;
+import one.digitalinnovation.gof.strategy.ComportamentoDefensivo;
+import one.digitalinnovation.gof.strategy.ComportamentoNormal;
+import one.digitalinnovation.gof.strategy.Robo;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class Test {
+    public static void main(String[] args) {
+        
+        // Singleton:
+        
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+        lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+        
+        // Strategy
+        
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento agressivo = new ComportamentoAgressivo();
+        
+        Robo robo = new Robo();
+        robo.setComportamento(normal);        
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(defensivo);        
+        robo.mover();
+        robo.setComportamento(agressivo);        
+        robo.mover();
+        robo.mover();
+        robo.mover();
+        
+        // Facade
+        
+        Facade facade = new Facade();
+        facade.migrarCLiente("Guilherme", "99999999");
+        
+    }
+}
---
+++ lab-padroes-projeto-java/src/one/digitalinnovation/gof/singleton/SingletonLazyHolder.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package one.digitalinnovation.gof.singleton;
+
+/**
+ * Singleton "Lazy Holder"
+ * @author Guilherme
+ */
+public class SingletonLazyHolder {
+    
+    private static class InstanceHolder{
+        public static SingletonLazyHolder instancia = new SingletonLazyHolder();
+    }
+    
+    private SingletonLazyHolder(){
+        super();
+    }
+    
+    public static SingletonLazyHolder getInstancia(){        
+        return InstanceHolder.instancia;
+    }
+    
+}
